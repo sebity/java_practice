a91de7209d2bc4fe32ad4be4e35e5e155b0863dc
Jan Tatham <jan@sebity.com>
2017-03-20 21:54:39 +0000

    Added Generic Part 2

---
+++ GenericsPart2/src/com/jantatham/BaseballPlayer.java
@@ -0,0 +1,10 @@
+package com.jantatham;
+
+/**
+ * Created by jan on 20/03/17.
+ */
+public class BaseballPlayer extends Player {
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
--- GenericsPart2/src/com/jantatham/Main.java
+++ GenericsPart2/src/com/jantatham/Main.java
@@ -4,4 +4,41 @@ package com.jantatham;
  * Created by jan on 20/03/17.
  */
 public class Main {
+    public static void main(String[] args) {
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team<FootballPlayer> chicagoBears = new Team<>("Chicago Bears");
+        chicagoBears.addPlayer(joe);
+
+        System.out.println(chicagoBears.numPlayers());
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("this won't work");
+        brokenTeam.addPlayer(beckham);
+
+        Team<FootballPlayer> detroitLions = new Team<>("Detroit Lions");
+        FootballPlayer jack = new FootballPlayer("Jack");
+        detroitLions.addPlayer(jack);
+
+        Team<FootballPlayer> dallasCowboys = new Team<>("Dallas Cowboys");
+        Team<FootballPlayer> nyjets = new Team<>("NY Jets");
+
+        dallasCowboys.matchResult(nyjets, 8, 0);
+        dallasCowboys.matchResult(detroitLions, 12, 8);
+        dallasCowboys.matchResult(chicagoBears, 4, 12);
+
+        System.out.println("Rankings");
+        System.out.println(dallasCowboys.getName() + ": " + dallasCowboys.ranking());
+        System.out.println(nyjets.getName() + ": " + nyjets.ranking());
+        System.out.println(detroitLions.getName() + ": " + detroitLions.ranking());
+        System.out.println(chicagoBears.getName() + ": " + chicagoBears.ranking());
+
+        System.out.println(dallasCowboys.compareTo(nyjets));
+        System.out.println(dallasCowboys.compareTo(detroitLions));
+        System.out.println(detroitLions.compareTo(chicagoBears));
+    }
 }
