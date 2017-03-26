946aea7ebcf5c656f639c109caeccab4a5215899
Jan Tatham <jan@sebity.com>
2017-03-26 13:18:28 +0100

    Added GameTest and Packages project

---
+++ Packages/src/com/jantatham/Main.java
@@ -0,0 +1,12 @@
+package com.jantatham;
+
+import javafx.scene.Node;
+
+/**
+ * Created by jan on 21/03/17.
+ */
+public class Main {
+    public static void main(String[] argv) {
+        Node node = null;
+    }
+}
