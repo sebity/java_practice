74c3394e68c9cf9ac6e97acaf6a0de5c0915b148
Jan Tatham <jan@sebity.com>
2017-04-10 23:40:37 +0100

    Added TodoList Project

---
+++ TodoList/src/com/jantatham/todolist/Controller.java
@@ -0,0 +1,75 @@
+package com.jantatham.todolist;
+
+import com.jantatham.todolist.datamodel.TodoItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+    private List<TodoItem> todoItems;
+    @FXML
+    private ListView<TodoItem> todoListView;
+    @FXML
+    private TextArea itemDetailsTextArea;
+    @FXML
+    private Label deadlineLabel;
+
+    public void initialize() {
+        TodoItem item1 =  new TodoItem("Mail birthday card", "Buy a 30th birthday card for John",
+                LocalDate.of(2016, Month.APRIL, 25));
+        TodoItem item2 =  new TodoItem("Dentist Appointment", "Get a checkup",
+                LocalDate.of(2016, Month.APRIL, 11));
+        TodoItem item3 =  new TodoItem("Kids back to school", "Kids return back to school",
+                LocalDate.of(2016, Month.APRIL, 19));
+        TodoItem item4 =  new TodoItem("Sebastien Violin Concert", "Sebastien's Prep School Orchestra",
+                LocalDate.of(2016, Month.MARCH, 25));
+        TodoItem item5 =  new TodoItem("Buy bread", "Got normal bread as there is none at home",
+                LocalDate.of(2016, Month.APRIL, 12));
+
+        todoItems = new ArrayList<>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+        todoItems.add(item3);
+        todoItems.add(item4);
+        todoItems.add(item5);
+
+        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
+            @Override
+            public void changed(ObservableValue<? extends TodoItem> observable, TodoItem oldValue, TodoItem newValue) {
+                if(newValue != null) {
+                    TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+                    itemDetailsTextArea.setText(item.getDetails());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+                    deadlineLabel.setText(df.format(item.getDeadline()));
+                }
+            }
+        });
+
+        todoListView.getItems().setAll(todoItems);
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        todoListView.getSelectionModel().selectFirst();
+    }
+
+    @FXML
+    public void handleClickListView() {
+        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+        itemDetailsTextArea.setText(item.getDetails());
+        deadlineLabel.setText(item.getDeadline().toString());
+//        System.out.println("The selected item is " + item);
+//        StringBuilder sb = new StringBuilder(item.getDetails());
+//        sb.append("\n\n\n\n");
+//        sb.append("Due: ");
+//        sb.append(item.getDeadline().toString());
+//        itemDetailsTextArea.setText(sb.toString());
+    }
+}
