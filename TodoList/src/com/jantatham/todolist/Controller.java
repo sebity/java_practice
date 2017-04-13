acd2eba46c7828268619546f6caf135cb4c51210
Jan Tatham <jan@sebity.com>
2017-04-14 00:23:48 +0100

    Added Toolbar

--- TodoList/src/com/jantatham/todolist/Controller.java
+++ TodoList/src/com/jantatham/todolist/Controller.java
@@ -9,6 +9,8 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.util.Callback;
@@ -91,7 +93,7 @@ public class Controller {
                             else {
                                 cell.setContextMenu(listContextMenu);
                             }
-            });
+                });
 
                 return cell;
             }
@@ -134,6 +136,16 @@ public class Controller {
 
         if(result.isPresent() && (result.get() == ButtonType.OK)) {
             TodoData.getInstance().deleteTodoItem(item);
+        }
+    }
+
+    @FXML
+    public void handleKeyPressed(KeyEvent keyEvent) {
+        TodoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
+        if(selectedItem != null) {
+            if(keyEvent.getCode().equals(KeyCode.DELETE)) {
+                deleteItem(selectedItem);
+            }
         }
     }
 
