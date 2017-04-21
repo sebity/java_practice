1b8ae2a238aa10c3d78c67fa3634bc4605c8de05
Jan Tatham <jan@sebity.com>
2017-04-21 23:05:42 +0100

    Added MyContacts and BankingTesting projects

---
+++ BankingTesting/src/com/jantatham/Main.java
@@ -0,0 +1,10 @@
+package com.jantatham;
+
+/**
+ * Created by jan on 17/04/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The application is running");
+    }
+}
---
+++ MyContacts/src/com/jantatham/Controller.java
@@ -0,0 +1,4 @@
+package com.jantatham;
+
+public class Controller {
+}
